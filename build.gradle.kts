340180a612183506ed86b3048444e54fa81853ce
Are Bjølseth <are.bjolseth@nav.com>
2020-03-04 13:54:59 +0100

    Added all files execept src/**

---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val dusseldorfKtorVersion = "1.3.0.b7013ab"
+val ktorVersion = ext.get("ktorVersion").toString()
+val mainClass = "no.nav.omsorgspenger.AppKt"
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+
+buildscript {
+    // Henter ut diverse dependency versjoner, i.e. ktorVersion.
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/b7013abce578447fb79186175b728e26e1537c27/gradle/dusseldorf-ktor.gradle.kts")
+}
+
+dependencies {
+    // Server
+    compile ( "no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
+    compile ( "no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
+    compile ( "no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
+    compile ( "no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
+    compile ( "no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
+    compile("io.ktor:ktor-locations:$ktorVersion")
+
+    // Client
+    compile ( "no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
+    compile ( "no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
+    compile ("io.lettuce:lettuce-core:5.2.1.RELEASE")
+    implementation("com.github.fppt:jedis-mock:0.1.16")
+
+    // Test
+    testCompile("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
+    testCompile("io.ktor:ktor-server-test-host:$ktorVersion") {
+        exclude(group = "org.eclipse.jetty")
+    }
+
+    testCompile ("org.skyscreamer:jsonassert:1.5.0")
+}
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/ktor")
+    maven("https://kotlin.bintray.com/kotlinx")
+    maven("http://packages.confluent.io/maven/")
+
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/navikt/dusseldorf-ktor")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+
+    jcenter()
+    mavenLocal()
+    mavenCentral()
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.named<KotlinCompile>("compileTestKotlin") {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("app")
+    archiveClassifier.set("")
+    manifest {
+        attributes(
+                mapOf(
+                        "Main-Class" to mainClass
+                )
+        )
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.6.2"
+}
