6489d79b9b4be101e50cf97204f7ad0b231ed1fe
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-03-30 20:24:14 +0200

    Legger til endepunkt for oms.utbetaling for arbeidstaker.

---
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/arbeidstakerutbetaling/Arbeidstakerutbetalingsøknad.kt
@@ -0,0 +1,15 @@
+package no.nav.omsorgspengerutbetaling.arbeidstakerutbetaling
+
+import no.nav.helse.arbeidsgiver.ArbeidsgiverDetaljer
+import no.nav.omsorgspengerutbetaling.felles.*
+
+data class Arbeidstakerutbetalingsøknad(
+    val språk: Språk,
+    val bosteder: List<Bosted>,
+    val opphold: List<Opphold>,
+    val spørsmål: List<SpørsmålOgSvar>,
+    val arbeidsgivere: ArbeidsgiverDetaljer,
+    val bekreftelser: Bekreftelser,
+    val utbetalingsperioder: List<UtbetalingsperiodeMedVedlegg>,
+    val fosterbarn: List<FosterBarn>? = listOf()
+)
--- src/main/kotlin/no/nav/omsorgspengerutbetaling/mottak/OmsorgpengesøknadMottakGateway.kt
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/mottak/OmsorgpengesøknadMottakGateway.kt
@@ -44,6 +44,11 @@ class OmsorgpengesøknadMottakGateway(
         pathParts = listOf("v1", "soknad")
     ).toString()
 
+    private val komplettArbeidstakerutbetalingUrl = Url.buildURL(
+        baseUrl = baseUrl,
+        pathParts = listOf("v1", "soknad/arbeidstaker")
+    ).toString()
+
     private val cachedAccessTokenClient = CachedAccessTokenClient(accessTokenClient)
 
     override suspend fun check(): Result {
@@ -67,6 +72,44 @@ class OmsorgpengesøknadMottakGateway(
         val contentStream = { ByteArrayInputStream(body) }
 
         val httpRequet = komplettUrl
+            .httpPost()
+            .timeout(20_000)
+            .timeoutRead(20_000)
+            .body(contentStream)
+            .header(
+                HttpHeaders.ContentType to "application/json",
+                HttpHeaders.XCorrelationId to callId.value,
+                HttpHeaders.Authorization to authorizationHeader,
+                apiGatewayApiKey.headerKey to apiGatewayApiKey.value
+            )
+
+        val (request, _, result) = Operation.monitored(
+            app = "omsorgspengerutbetaling-api",
+            operation = "sende-soknad-til-prosessering",
+            resultResolver = { 202 == it.second.statusCode }
+        ) { httpRequet.awaitStringResponseResult() }
+
+        result.fold(
+            { },
+            { error ->
+                logger.error("Error response = '${error.response.body().asString("text/plain")}' fra '${request.url}'")
+                logger.error(error.toString())
+                throw IllegalStateException("Feil ved sending av søknad til prosessering.")
+            }
+        )
+    }
+
+    suspend fun leggTilProsessering(
+        soknad: KomplettArbeidstakerutbetalingsøknad,
+        callId: CallId
+    ) {
+        val authorizationHeader =
+            cachedAccessTokenClient.getAccessToken(sendeSoknadTilProsesseringScopes).asAuthoriationHeader()
+
+        val body = objectMapper.writeValueAsBytes(soknad)
+        val contentStream = { ByteArrayInputStream(body) }
+
+        val httpRequet = komplettArbeidstakerutbetalingUrl
             .httpPost()
             .timeout(20_000)
             .timeoutRead(20_000)
