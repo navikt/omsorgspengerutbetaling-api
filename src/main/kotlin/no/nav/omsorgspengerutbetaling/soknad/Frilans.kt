d5c2f44d4aa452ea8f90aa5b3eca9f3b9abfd673
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-03-22 11:29:53 +0100

    Legger på sluttdato på frilanser.

--- src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/Frilans.kt
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/Frilans.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class  Frilans(
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    val startdato: LocalDate,
+    @JsonFormat(pattern = "yyyy-MM-dd") val startdato: LocalDate,
+    @JsonFormat(pattern = "yyyy-MM-dd") val sluttdato: LocalDate? = null,
     val jobberFortsattSomFrilans: JaNei
 )
 
