5a35e370fdafaab77111ed2ead03fe5cd76ae671
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-03-29 10:21:53 +0200

    Diverse oppgraderinger. (#48)
    - Oppgraderer dusseldorf-ktor.
    - Oppgraderer kotlin jvm (closes #44).
    - Oppgraderer gradle wrapper.

--- src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/OmsorgpengesøknadMottakGateway.kt
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/OmsorgpengesøknadMottakGateway.kt
@@ -1,12 +1,11 @@
 package no.nav.omsorgspengerutbetaling.soknad
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import com.github.kittinunf.fuel.httpPost
-import io.ktor.http.HttpHeaders
-import io.ktor.http.Url
+import io.ktor.http.*
 import no.nav.helse.dusseldorf.ktor.client.buildURL
 import no.nav.helse.dusseldorf.ktor.health.HealthCheck
 import no.nav.helse.dusseldorf.ktor.health.Healthy
@@ -33,7 +32,7 @@ class OmsorgpengesøknadMottakGateway(
     private companion object {
         private val logger: Logger = LoggerFactory.getLogger(OmsorgpengesøknadMottakGateway::class.java)
         private val objectMapper = jacksonObjectMapper().dusseldorfConfigured()
-            .setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE)
+            .setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
             .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
     }
 
