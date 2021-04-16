cb6a7a0dcbe6d9469e59bed680ccf6cec295cb89
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-04-16 10:03:41 +0200

    Fikser oppkobling for redisClient mot redisServer i test. (#54)

--- src/main/kotlin/no/nav/omsorgspengerutbetaling/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/redis/RedisConfig.kt
@@ -2,12 +2,13 @@ package no.nav.omsorgspengerutbetaling.redis
 
 import io.ktor.util.KtorExperimentalAPI
 import io.lettuce.core.RedisClient
+import io.lettuce.core.RedisURI
+import java.time.Duration
 
 internal object RedisConfig {
 
     @KtorExperimentalAPI
     internal fun redisClient(redisHost: String, redisPort: Int): RedisClient {
-        return RedisClient.create("redis://${redisHost}:${redisPort}")
+        return RedisClient.create(RedisURI(redisHost, redisPort, Duration.ofSeconds(60)))
     }
-
 }
