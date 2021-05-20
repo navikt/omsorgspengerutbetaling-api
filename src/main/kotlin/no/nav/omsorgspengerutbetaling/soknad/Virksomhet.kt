84d3fada89693b91e68c15217b56b9a09cb273e8
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-05-20 11:07:41 +0200

    Enabler validering og test nå som frontend er oppdatert

--- src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/Virksomhet.kt
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/Virksomhet.kt
@@ -78,7 +78,6 @@ internal fun Virksomhet.validate(index: Int): MutableSet<Violation> {
         )
     }
 
-    /* //TODO 20/05/2021 - Skru på validering når frontend er prodsatt
     if(harFlereAktiveVirksomheter == null){
         violations.add(
             Violation(
@@ -89,7 +88,6 @@ internal fun Virksomhet.validate(index: Int): MutableSet<Violation> {
             )
         )
     }
-     */
 
     return violations
 }
